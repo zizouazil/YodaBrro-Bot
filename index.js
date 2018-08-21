989ed05c3a33f9a1478d221f7516ddcea570815e
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-21 09:59:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1298,25 +1298,6 @@ LET'S GO!
 
 });
 
-client.on('ready', function(){
-    client.user.setStatus("dnd");
-    var ms = 10000 ;
-    var setGame = [`${PREFIX}help`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`https://www.twitch.tv/skwadraa`);
-    }, ms);10000
-
-});
-
 client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
 
 client.on('reconnecting', () => console.log('I am reconnecting now!'));
