bca8dc12220a58cf30b4ddc580126f2f7d9797ce
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-22 17:44:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2168,4 +2168,32 @@ client.on('message', async message => {
   }
 });
 
+client.on('message',message =>{
+    var prefix = "/";
+    if(message.content.startsWith('!top')) {
+  message.guild.fetchInvites().then(i =>{
+  var invites = [];
+   
+  i.forEach(inv =>{
+    var [invs,i]=[{},null];
+     
+    if(inv.maxUses){
+        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
+    }else{
+        invs[inv.code] =+ inv.uses;
+    }
+        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
+   
+  });
+  var embed = new Discord.RichEmbed()
+  .setColor("#000000")
+  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
+  .setThumbnail("https://i.imgur.com/OM00xyh.png")
+           message.channel.send({ embed: embed });
+   
+  });
+   
+    }
+  });
+
 client.login(process.env.BOT_TOKEN)
