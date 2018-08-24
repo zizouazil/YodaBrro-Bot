3e28f34c0960309f153e877e10fd9a3eb5c39b0b
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-24 16:21:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3626,4 +3626,15 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**You
 
 });
 
+client.on('message', function(message) {
+    if(message.content.startsWith('!role')) {
+        let guild = message.mentions.members.first();
+                          let ZmA = new Discord.RichEmbed()
+                  .setColor('3fcf24')
+                  .setDescription('**__:white_check_mark: تم إضافة رتبة لك__**')
+        message.member.addRole(message.guild.roles.find('name', 'اسم الرتبه'));
+                    message.channel.send({embed:ZmA});
+    }
+});
+
 client.login(process.env.BOT_TOKEN)
