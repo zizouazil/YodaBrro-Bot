5e2428d3e676b8eac22785387d70389250ea0506
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-24 16:53:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3665,28 +3665,6 @@ client.on('message', message => {
     }
 });
 
-client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | You Have been Punished")
-            .addField(`** You have broke the rules by Advertising  **` , `**Note  : If this mute was wrong, Please talk to Admin**`)
-      .addField(`by`,`shyboy_05`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL) 
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500) 
-    
-        
-    }
-    }
-})
-
 client.on(`message`, message => {
     var args = message.content.split(/[ ]+/)
     if(message.content.includes(`youtube`)){
