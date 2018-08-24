3d8deb0bdb44dcb991efaaa2eab066817cfe383d
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-08-24 17:58:56 +0400

    Update index.js

--- index.js
+++ index.js
@@ -569,29 +569,86 @@ if (message.content.startsWith("!cv")) {
   message.channel.send({embed});
      }
  });
- 
- client.on('message', (message) => {
-   if (message.content.startsWith('!kick')) {
-       var member= message.mentions.members.first();
-       member.kick().then((member) => {
-           message.channel.send(member.displayName + ' This user has been Kicked');
-       }).catch(() => {
-           message.channel.send(":x:");
-       });
-   }
+
+ var prefix = "f!"
+ client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "chat");
+
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**Mention a Member**");
+  if(!reason) return message.reply ("**Write Ban Reason**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**I can not kick someone higher than me **");
+
+  message.guild.member(user).ban(7, user);
+
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(`Banned!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : banembed
+  })
+}
 }); 
 
 
-client.on('message', (message) => {
-   if (message.content.startsWith('!ban ')) {
-     if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You must have Ban Members Permissions');
-       var member= message.mentions.members.first();
-       member.ban().then((member) => {
-        message.channel.send(member.displayName + 'This user hase been banned');
-       }).catch(() => {
-           message.channel.send('Error :_:');
-       });
-   }
+var prefix = "f!"
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "chat");
+
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**Mention a Member**");
+  if(!reason) return message.reply ("**Write Ban Reason**");
+  if (!message.guild.member(user)
+  .kickable) return message.reply("**I can not ban someone higher than me**");
+
+  message.guild.member(user).kick();
+
+  const kickembed = new Discord.RichEmbed()
+  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
+  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
+  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
+  message.channel.send({
+    embed : kickembed
+  })
+}
 });
  
  client.on("message", (message) => {
@@ -2469,10 +2526,7 @@ embed.addField(":cloud_tornado:  الاسم", `**[ ${msg.author.username}#${msg.
    .addField(':spy:  Status', `**[ ${msg.author.presence.status.toUpperCase()} ]**`, true)
    .addField(':satellite_orbital:   Playing', `**[ ${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name} ]**`, true)
    .addField(':military_medal:  Roles', `**[ ${msg.member.roles.filter(r => r.name).size} ]**`, true)
-   .addField(':robot:  Is he Bot?', `**[ ${msg.author.bot.toString().toUpperCase()} ]**`, true);
-   .addField(': Joined the Server before', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-   .addField(': Joined the Discord before', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
-   .addField(": Nickname",`${h.nickname}`, true) .addField(": #",heg.discriminator, true)
+   .addField(':robot:  Is he Bot?', `**[ ${msg.author.bot.toString().toUpperCase()} ]**`, true)
 msg.channel.send({embed: embed})
 }
 });  
