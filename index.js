51ee7a5cddaf87c6fcf3604ab8f39588edab8943
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-12 17:08:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2875,20 +2875,4 @@ client.users.filter(u => u.discriminator == message.author.discriminator).forEac
 }
 });
 
-client.on("message", message => {
-        if (message.content === "!setprefix") {
-        if (message.author.id !== "274923685985386496" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
-  let args = message.content.split(" ").slice(1);
-        let arg = args.join("").substring(message.length)
-        if (!arg) return message.channel.send(`Please add a prefix after command like \`\`${prefix}setprefix &\`\``);
-        fs.database().ref('servers/' + message.guild.id).update({
-            guildname: message.guild.name,
-            guildprefix: arg
-        }).catch(function(err) {
-            message.channel.send(err + "\n\n\n");
-        });
-        message.channel.send(`prefix updated ${arg} for ${message.guild.name}`);
-    }
-});
-
 client.login(process.env.BOT_TOKEN)
