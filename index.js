00316789c3ccddfba4cf67daf212f93d00223c5e
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-12 21:16:24 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2922,4 +2922,158 @@ client.on('message', message => {
  
  });
 
+let id;
+let guild;
+let emojiHasPermission;
+
+client.on('message', message => {
+  id = client.user.id;
+  guild = message.guild;
+  emojiHasPermission = permission => {
+    console.log(permission);
+    if (guild.member(id).permissions.has(permission, false)) {
+      return ':white_check_mark:';
+    } else {
+      return ':x:';
+    }
+  };
+  if (message.content === '!permissions') {
+  message.channel.send({embed: {
+    title: ':tools: Permissions',
+    color: 0x06DF00,
+    fields: [
+      {
+
+        name: 'Administrator :',
+        value: emojiHasPermission('ADMINISTRATOR'),
+        inline: true
+      },
+      {
+        name: 'Create Instant Invite :',
+        value: emojiHasPermission('CREATE_INSTANT_INVITE'),
+        inline: true
+      },
+      {
+        name: 'Kick Members :',
+        value: emojiHasPermission('KICK_MEMBERS'),
+        inline: true
+      },
+      {
+        name: 'Ban Members :',
+        value: emojiHasPermission('BAN_MEMBERS'),
+        inline: true
+      },
+      {
+        name: 'Manage Channels :',
+        value: emojiHasPermission('MANAGE_CHANNELS'),
+        inline: true
+      },
+      {
+        name: 'Manage Guild :',
+        value: emojiHasPermission('MANAGE_GUILD'),
+        inline: true
+      },
+      {
+        name: 'Add Reactions :',
+        value: emojiHasPermission('ADD_REACTIONS'),
+        inline: true
+      },
+      {
+        name: 'View Audit Log :',
+        value: emojiHasPermission('VIEW_AUDIT_LOG'),
+        inline: true
+      },
+      {
+
+        name: 'Manage Messages :',
+        value: emojiHasPermission('MANAGE_MESSAGES'),
+        inline: true
+      },
+      {
+        name: 'Embed Links :',
+        value: emojiHasPermission('EMBED_LINKS'),
+        inline: true
+      },
+      {
+        name: 'Attach Files :',
+        value: emojiHasPermission('ATTACH_FILES'),
+        inline: true
+      },
+      {
+        name: 'Read Message History :',
+        value: emojiHasPermission('READ_MESSAGE_HISTORY'),
+        inline: true
+      },
+      {
+        name: 'Mention Everyone :',
+        value: emojiHasPermission('MENTION_EVERYONE'),
+        inline: true
+      },
+      {
+        name: 'Use External Emojis :',
+        value: emojiHasPermission('USE_EXTERNAL_EMOJIS'),
+        inline: true
+      },
+      {
+        name: 'Connect :',
+        value: emojiHasPermission('CONNECT'),
+        inline: true
+      },
+      {
+        name: 'Speak :',
+        value: emojiHasPermission('SPEAK'),
+        inline: true
+      },
+      {
+
+        name: 'Mute Members :',
+        value: emojiHasPermission('MUTE_MEMBERS'),
+        inline: true
+      },
+      {
+        name: 'Deafen Members :',
+        value: emojiHasPermission('DEAFEN_MEMBERS'),
+        inline: true
+      },
+      {
+        name: 'Move Members :',
+        value: emojiHasPermission('MOVE_MEMBERS'),
+        inline: true
+      },
+      {
+        name: 'Use VAD :',
+        value: emojiHasPermission('USE_VAD'),
+        inline: true
+      },
+      {
+        name: 'Change Nickname :',
+        value: emojiHasPermission('CHANGE_NICKNAME'),
+        inline: true
+      },
+      {
+        name: 'Manage Nicknames :',
+        value: emojiHasPermission('MANAGE_NICKNAMES'),
+        inline: true
+      },
+      {
+        name: 'Manage Roles :',
+        value: emojiHasPermission('MANAGE_ROLES'),
+        inline: true
+      },
+      {
+        name: 'Manage Webhooks :',
+        value: emojiHasPermission('MANAGE_WEBHOOKS'),
+        inline: true
+      },
+      {
+        name: 'Manage Emojis :',
+        value: emojiHasPermission('MANAGE_EMOJIS'),
+        inline: true
+      }
+    ]
+  }
+  });
+  }
+});
+
 client.login(process.env.BOT_TOKEN)
