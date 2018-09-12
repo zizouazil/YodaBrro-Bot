45d36710eedacd2ea32504949f52f8c71421851b
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-12 21:03:10 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2892,7 +2892,7 @@ client.users.filter(u => u.discriminator == message.author.discriminator).forEac
 });
 
 client.on('message', message => {
-         if (message.content === prefix + "dt") {
+         if (message.content === "!dt") {
          if (!message.channel.guild) return message.reply('** This command only for servers **');  
          var currentTime = new Date(),
             hours = currentTime.getHours() + 4 ,
@@ -2952,6 +2952,29 @@ client.on('message', message => {
 
                  message.channel.sendEmbed(Date15);
         }
-    });
+});
+
+client.on("message", message => {
+    const command = message.content.split(" ")[0];
+    const args = message.content.split(" ").slice();
+  let container = message.content.split(" ");
+  let result =  Number(container[1]) + Number(container[3]);
+  let result2 = Number(container[1]) - Number(container[3]);
+  let result3 = Number(container[1]) * Number(container[3]);
+  let result4 = Number(container[1]) / Number(container[3]);
+  if(command === "!calculate"){
+if(!args[3]) return;
+if(!args[2]) return;
+    if (container[2] === "+") {
+        message.channel.send("Answer is " + result);
+        } else if (container[2] === "-") {
+          message.channel.send("Answer is " + result2);
+        } else if (container[2] === "*" || container[2].toLowerCase() === "x" || container[2].toLowerCase() === "×") {
+          message.channel.send("Answer is " + result3);
+        } else if (container[2] === "/" || container[2].toLowerCase() == "÷") {
+          message.channel.send("Answer is " + result4);
+        }
+        }
+});//.catch(console.error);
 
 client.login(process.env.BOT_TOKEN)
