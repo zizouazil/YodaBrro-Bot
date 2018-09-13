303bc6022c389827dc6092c5151ca21e69293e2d
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-14 00:51:07 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3351,34 +3351,4 @@ hours = 12;
   }
 });
 
-client.on('message', message => {
-	let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
- 
-   let args = message.content.split(" ").slice(1);
- 
- 
- if(command == "!draw") {
-   , Image = new Canvas.Image
-   , canvas = new Canvas(450, 170)
-   , ctx = canvas.getContext('2d');
-   ctx.font = '30px Impact';
-   let args = message.content.split(" ").slice(1);
-   
- Image.src = canvas.toBuffer();
- 
-     console.log(Image);
- ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
- ctx.fillText(args.join("  "),110, 70);
- 
- 
- ctx.beginPath();
- ctx.lineTo(50, 102);
- ctx.stroke();
- 
- message.channel.sendFile(canvas.toBuffer());
- }
- 
- });
-
 client.login(process.env.BOT_TOKEN)
