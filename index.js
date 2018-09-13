2ba987500a36deaa319110aea2636fdac7632083
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-13 11:25:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3236,47 +3236,4 @@ client.on('message', msg => {
   msg.reply('**Welcome to ${guild.name}**')
 });
 
-client.on("message", message => {
- 
-  function discoRole() {
-    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    roles.forEach((role) => {
-      let theRole = message.guild.roles.find("name", role);
-      theRole.edit({color: random}).catch(e => {
-        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
-      });
-    });
-  }
- 
-  if(message.content.startsWith("!startdisco")) {
-    if(allowedUsers.includes(message.author.id)) {
-    setInterval(() => { discoRole(); }, config.ms);
-    message.channel.send("```css\nDiscoing...```");
-    message.channel.send("Please make sure you read the README, you could get IP banned from discord because of ratelimits.");
-  } else {
-    message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-} else
- 
-if(message.content.startsWith("!stopdisco")) {
-  if(allowedUsers.includes(message.author.id)) {
-  message.channel.send("I've stopped discoing.");
-  setTimeout(() => { console.log(process.exit(0)); }, 300);
-} else {
-  message.reply(`You do not have permission to disco. If you have downloaded this bot off of github please go to the config.json and add your user ID to the "allowedUsers" value.`);
-  }
-}
- 
-});
-const config = require('config.json');
-const prefix = config.prefix;
- 
-const allowedUsers = config.allowedUsers;
-const roles = config.roleToDisco;
- 
-client.on("ready", () => {
-    client.user.setPresence({ game: { name: `Disco Roles! Created by i am toast#1213` }, type: 0 });
-    console.log("Disco role bot online! Created by i am toast.");
-});
-
 client.login(process.env.BOT_TOKEN)
