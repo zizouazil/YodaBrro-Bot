2ad59d471a67c648fb7dbb0cf18907aa3455d751
HeemPlayz <42186537+HeemPlayz@users.noreply.github.com>
2018-09-14 15:12:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4050,7 +4050,7 @@ client.on('message', message => {
 client.on("message", message => {
   if (message.author.bot) return;
  if(!message.channel.guild)return;
-  if (!profile[message.author.id]) profile[message.author.id] = {
+  if (!client[message.author.id]) client[message.author.id] = {
     tite: 'HypeLC User',
     rep: 0,
    reps: 'NOT YET',
@@ -4070,7 +4070,7 @@ client.on("message", (message) => {
     if(!message.channel.guild) return;
 if (message.content.startsWith(prefix + 'credits')) {
   if(men) {
-  if (!profile[men.id]) profile[men.id] = {
+  if (!client[men.id]) client[men.id] = {
    lastDaily:'Not Collected',
    credits: 1,
  };
@@ -4084,9 +4084,9 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 if(message.content.startsWith(prefix + "daily")) {
 
 
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
-   profile[message.author.id].lastDaily = moment().format('day')
-   profile[message.author.id].credits += 310
+  if(client[message.author.id].lastDaily != moment().format('day')) {
+   client[message.author.id].lastDaily = moment().format('day')
+   client[message.author.id].credits += 310
     message.channel.send(`**${message.author.username} you collect your \`310\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
@@ -4105,7 +4105,7 @@ if(message.content.startsWith(prefix + 'trans')) {
             message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
             return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
              }
-             if(profile[message.author.id].credits < args[0]) return message.channel.send("**Your Credits is not enough  that**")
+             if(client[message.author.id].credits < args[0]) return message.channel.send("**Your Credits is not enough  that**")
 if(args[0].startsWith("-")) return  message.channel.send('**!! I Cant Do it**');
 				 let defineduser = '';
             let firstMentioned = message.mentions.users.first();
@@ -4113,9 +4113,9 @@ if(args[0].startsWith("-")) return  message.channel.send('**!! I Cant Do it**');
             if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
             if(defineduser.id === message.author.id) return message.channel.send("***Transfering to your self hah ?!***")
             var mentionned = message.mentions.users.first();
-if (!profile[sender.id]) profile[sender.id] = {}
-if (!profile[sender.id].credits) profile[sender.id].credits = 310;
-fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
+if (!client[sender.id]) client[sender.id] = {}
+if (!client[sender.id].credits) client[sender.id].credits = 310;
+fs.writeFile('./profile.json', JSON.stringify(client), (err) => {
 if (err) console.error(err);
 })
 var x = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' , '3489' , '1382' , '7389' , '8762' , '0889' , '0388' , '3316' , '0976' , '8603' , '1842' , '4565' , '9524' , '9524' , '0964' , '5930' , '5678' , '9567' , '6099' , '7058' , '0001' , '1324' , '9834' , '7668' , '0378' , '7055' , '9733' , '9876' , '9846' , '9685' , '8574' , '8975' , '9845' , '9862' , '0069' , '0807' , '0673' , '0813' , '1235' , '6879'];
@@ -4131,10 +4131,10 @@ var x2 = ['5587' ,' 9978' , '3785' , '7734' , '9864' , '7681' , '3758' , '7834' 
         })
         r.then(s=> {
       var mando = message.mentions.users.id;
-      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
-      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
-      profile[defineduser.id].credits += (+args[0]);
-      profile[sender.id].credits += (-args[0]);
+      if  (!client[defineduser.id]) client[defineduser.id] = {}
+      if (!client[defineduser.id].credits) client[defineduser.id].credits = 200;
+      client[defineduser.id].credits += (+args[0]);
+      client[sender.id].credits += (-args[0]);
       let mariam = message.author.username
 message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
 mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${args[0]} from user ${message.author.username} ; (ID (${message.author.id})\`\`\``);
