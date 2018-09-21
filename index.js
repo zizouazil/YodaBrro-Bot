980dd90af56271522a5a27784b08c85f2c709947
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-09-21 09:01:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4704,7 +4704,7 @@ client.on('message',async msg => {
 Year = currentTime.getFullYear(),
 Month = currentTime.getMonth() + 1,
 Dat = currentTime.getDate()
-      time.setName(`Members : ◤ → ${message.guild.memberCount} ← ◢`);
+      time.setName(`Members : ◤ → ${guild.memberCount} ← ◢`);
  },1000);
   });
   }
