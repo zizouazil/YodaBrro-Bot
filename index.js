7089c7c87eeb411c87ec0b201116dbfd381b797c
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-09-21 19:48:33 +0400

    Update index.js

--- index.js
+++ index.js
@@ -64,6 +64,21 @@ client.on('message', message => {
    }
 });
 
+client.on('message', async message => {
+  if(message.content.startsWith("!bcall")) {
+    let i = client.users.size;
+    if(message.author.id !== '441963199462506508') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
+
 
 console.log('mariam ra7t tmot al nas');
 client.on('ready', () => {
