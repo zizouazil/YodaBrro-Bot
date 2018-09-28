e1d33f69c3bbebcc5e5be9ed007f5e2d58cc71b2
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-09-28 17:51:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ client.on('ready', () => {
           client.user.setActivity("By: YodaBrro#4557",{type: 'STREAMING'});     //Heeeeeemo    
 });
 
+client.on('message', message => {
+   if(message.content.startsWith("!invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has **${inviteCount}** invites.`);
+});
+  }
+});
+
 const code = '!';
 
 client.on('message',async message => {
